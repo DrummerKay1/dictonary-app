f8a79422371489b38c8acdf47b6c60b34d123f34
DrummerKay1 <96755662+DrummerKay1@users.noreply.github.com>
2022-04-30 19:55:38 -0500

    Added alt

--- src/Photos.js
+++ src/Photos.js
@@ -11,11 +11,10 @@ export default function Photos(props) {
               <div className="col-4" key={index}>
                 <a
                   href={photo.src.original}
-                  alt=""
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <img src={photo.src.landscape} className="img-fluid" />{" "}
+                  <img src={photo.src.landscape} className="img-fluid" alt={photo.src.photographer}/>{" "}
                 </a>
               </div>
             );
